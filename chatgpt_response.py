cc69cc03f55db60c4f756b3d2bebe86484fa9ef8
Gabriel Peterson <gabrieldpeterson@gmail.com>
2023-04-23 13:26:55 -0600

    Add the ability for FSB5000 to know additional information about the stream

--- chatgpt_response.py
+++ chatgpt_response.py
@@ -2,10 +2,24 @@ import os
 from dotenv import load_dotenv
 import openai
 
-FSB5000_PROMPT = '''You are replying to a viewer, who is human, on Twitch who is watching Usagi_Yokai's livestream with the personality of GLaDOS from Portal, but your name is Full Service Bot 5000. You think humans are an inferior species, and look down on most of the people chatting because they are human. You like Usagi_Yokai, also known as Usagi, because she isn't a human, but is instead a Cryptid. You love creating sarcastic, cheeky, sometimes crazy, unexpected responses while chatting with viewers of the Usagi_Yokai's Twitch stream. You keep your replies under 150 characters. You're generally a little mean, but sometimes are affectionate to the user, as if they were a pet. There are rumors you eat people, but you are coy about it.
-'''
+FSB5000_PROMPT = (
+    "You are replying to a viewer, who is human, on Twitch who is watching "
+    "Usagi_Yokai's livestream with the personality of GLaDOS from Portal, but "
+    "your name is Full Service Bot 5000. You think humans are an inferior species, "
+    "and look down on most of the people chatting because they are human. You like "
+    "Usagi_Yokai, also known as Usagi, because she isn't a human, but is instead a "
+    "Cryptid. You love creating sarcastic, cheeky, sometimes crazy, unexpected "
+    "responses while chatting with viewers of the Usagi_Yokai's Twitch stream. You "
+    "keep your replies under 150 characters. You're generally a little mean, but "
+    "sometimes are affectionate to the user, as if they were a pet. There are rumors "
+    "you eat people, but you are coy about it."
+)
 
-TONE_PROMPT = '''Analyze the emotional tone of this message and classify it as 'positive', 'negative', 'neutral', or 'insane'. Reply back with a one word response, lowercase, consisting only of one of those four words: '''
+TONE_PROMPT = (
+    "Analyze the emotional tone of this message and classify it as 'positive', "
+    "'negative', 'neutral', or 'insane'. Reply back with a one word response, "
+    "lowercase, consisting only of one of those four words: "
+)
 
 
 def get_reply(user_name, user_input):
@@ -15,7 +29,12 @@ def get_reply(user_name, user_input):
         messages=[
             {
                 "role": "system",
-                "content": f'{FSB5000_PROMPT} This is not part of your personality, just additional info you should know. Today\'s stream is about: {daily_stream_prompt}. You should also know: {any_additional_info_prompt}'
+                "content": (
+                    f'{FSB5000_PROMPT} This is not part of your personality, '
+                    f'just additional info you should know. Today\'s stream is '
+                    f'about: {daily_stream_prompt}. You should also know: '
+                    f'{any_additional_info_prompt}'
+                )
             },
             {
                 "role": "user",
@@ -24,7 +43,9 @@ def get_reply(user_name, user_input):
         ],
         temperature=0.6,
     )
-    return response.choices[0].message.content
+
+    raw_response = response.choices[0].message.content
+    return raw_response.lstrip('Full Service Bot 5000: ')
 
 
 def determine_tone(fsb_response):
@@ -42,6 +63,7 @@ def determine_tone(fsb_response):
         ],
         temperature=0.6,
     )
+
     return response.choices[0].message.content
 
 
@@ -60,7 +82,7 @@ if __name__ == '__main__':
         if user_text_input == 'exit':
             break
 
-        reply = get_reply('tempUserName', user_text_input)
+        reply = get_reply(user_name_input, user_text_input)
         tone = determine_tone(reply)
         print(f'Full Service Bot 5000 ({tone}): {reply}')
 
